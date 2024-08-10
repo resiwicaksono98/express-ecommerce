4f26d79fde9215bf26a5981939b9337eadb2aed8
Resi Wicaksono <resiwicaksono98@gmail.com>
2024-08-10 13:06:24 +0700

    initialize project

---
+++ src/application/app.ts
@@ -0,0 +1,7 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+
+export const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
